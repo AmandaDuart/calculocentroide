e54bcfe2a3d7b0f552eb8aeaeffdbd70fb9dfa10
Amanda <amandaduartdasilva@gmail.com>
2019-10-13 20:24:28 -0300

    comitando def distancia

--- 3Quest.py
+++ 3Quest.py
@@ -2,10 +2,7 @@
 
 def converteNum(s):
 	ponto_c = s.split(' ')
-	try:
-		return (float(ponto_c[0])),(float(ponto_c[1]))
-	except ValueError:
-		return float(s)
+	return (float(ponto_c[0])),(float(ponto_c[1]))
 
 def centroide(total_x, total_y, p):
 	centro_x = total_x / p
@@ -15,16 +12,12 @@ def centroide(total_x, total_y, p):
 def distancia(x_p, y_p, x_c, y_c):
 	lista_xy = []
 	distancias= {}
-	menor = float(0)
+	menor = float(99999999)
 	for i in range(0, len(x_p)):
 		distancia_xy = (((x_c - x_p[i])**2) + (y_c - y_p[i]) ** 2) ** 0.5 
 		lista_xy.append(distancia_xy)
-		distancias = lista_xy[i], x_p[i], y_p[i]
-		if menor == 0:
+		if lista_xy[i]< menor:
 			menor = lista_xy[i]
-		else:
-			if lista_xy[i]< menor:
-				menor = lista_xy[i]
 	for p , v in enumerate(lista_xy):
 		if v == menor:
 			posição = p
